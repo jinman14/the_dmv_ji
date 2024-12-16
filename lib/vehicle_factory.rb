dd4b8cdb7d036c85e0855a286d721eb12404ff28
Jacob Inman <jrinman6295@gmail.com>
2024-12-16 11:21:58 -0800

    vehicle_factory class complete, starting spec

---
+++ lib/vehicle_factory.rb
@@ -0,0 +1,22 @@
+class VehicleFactory
+    attr_reader :cars
+
+    def initialize(cars)
+        @cars = []
+    end
+
+    def create_vehicles(vehicle_details)
+
+        vehicle_details.each do |vehicle|
+            new_vehicle = Vehicle.new({
+                :engine => :ev,
+                :make => vehicle[:make],
+                :model => vehicle[:model],
+                :vin => vehicle[:vin_1_10],
+                :year => vehicle[:model_year]
+            })
+
+            @cars << new_vehicle
+        end
+    end
+end
