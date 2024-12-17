df6c9231d1886365930ba17b4e9ee0f9ac5f55b9
Jacob Inman <jrinman6295@gmail.com>
2024-12-16 16:38:38 -0800

    other offices data now able to be read

--- lib/dmv_facilities.rb
+++ lib/dmv_facilities.rb
@@ -8,11 +8,12 @@ class DMVFacility
     def create_facility(office_details)
 
         office_details.each do |office|
-            new_facility = Facility.new({
-                :name => office[:dmv_office],
-                :address => office[:address_li],
-                :phone => office[:phone]
-            })
+
+                new_facility = Facility.new({
+                    :name => "#{office[:dmv_office]}#{office[:office_name]}#{office[:name]}",
+                    :address => "#{office[:address_li]}#{office[:street_address_line_1]}#{office[:address1]}",
+                    :phone => "#{office[:phone]}#{office[:public_phone_number]}"
+                    })
 
             @dmv_facility << new_facility
         end
--- spec/dmv_facilities_spec.rb
+++ spec/dmv_facilities_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe DMVFacility do
     before(:each) do
         @dmv_facility_1 = DMVFacility.new
         @dmv_facility_2 = DMVFacility.new
+        @dmv_facility_3 = DMVFacility.new
         @co_dmv_office_locations = DmvDataService.new.co_dmv_office_locations
         @ny_dmv_office_locations = DmvDataService.new.ny_dmv_office_locations
         @mo_dmv_office_locations = DmvDataService.new.mo_dmv_office_locations
@@ -20,17 +21,29 @@ RSpec.describe DMVFacility do
         it 'can collect dmv office data from external source' do
             expect(@dmv_facility_1.dmv_facility).to eq([])
 
-            @dmv_facility.create_facility(@co_dmv_office_locations)
+            @dmv_facility_1.create_facility(@co_dmv_office_locations)
             # binding.pry
             expect(@dmv_facility_1.dmv_facility[0].name).to eq("DMV Tremont Branch")
             expect(@dmv_facility_1.dmv_facility[0].address).to eq("2855 Tremont Place")
             expect(@dmv_facility_1.dmv_facility[0].phone).to eq("(720) 865-4600")
         end
-    end
 
-        xit 'can collect from other facilities' do
+        it 'can collect from other facilities' do
+
             @dmv_facility_2.create_facility(@ny_dmv_office_locations)
-
-            binding.pry
+            # binding.pry
+            expect(@dmv_facility_2.dmv_facility[0].name).to eq("LAKE PLACID")
+            expect(@dmv_facility_2.dmv_facility[0].address).to eq("2693 MAIN STREET")
+            expect(@dmv_facility_2.dmv_facility[1].phone).to eq("5188283350")
         end
+
+        it 'can collect from missouri too' do
+
+            @dmv_facility_3.create_facility(@mo_dmv_office_locations)
+
+            expect(@dmv_facility_3.dmv_facility[0].name).to eq("Harrisonville")
+            expect(@dmv_facility_3.dmv_facility[0].address).to eq("2009 Plaza Dr.")
+            expect(@dmv_facility_3.dmv_facility[0].phone).to eq("(816) 884-4133")
+        end
+    end
 end
